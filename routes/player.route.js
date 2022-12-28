be5dfd070af11ca1410b4dea6aae0a5eb9175652
JusAeng <justo_play@hotmail.com>
2022-12-29 03:34:50 +0700

    add manage api player

--- routes/player.route.js
+++ routes/player.route.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
 
+
+//get All players
 router.get('/',(req,res) => {
-    res.send('here player')
-})
+    res.send('here player');
+});
+
+//get Player by Id
+router.get('/:id',(req,res) => {
+    res.send("brabra");
+});
+
+//add Player
+router.post('/add',(req,res) => {
+    res.send("player created !");
+});
+
+//delete Player by Id
+router.delete('/delete/:id',(req,res) => {
+    res.send("player deleted !");
+});
+
+//update Player by Id
+router.put('/update/:id', (req,res) => {
+    res.send('player updated !');
+});
+
 
 module.exports = router;
