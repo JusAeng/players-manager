1a80768ad3666836d5af5b1f212310e594adee22
JusAeng <justo_play@hotmail.com>
2023-01-08 18:33:39 +0700

    mod schema

--- models/player.model.js
+++ models/player.model.js
@@ -4,7 +4,45 @@ const playerSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-    }},{
+    },
+    position: {
+        type: String,
+        require: true,
+    },
+    class: {
+        type: Number,
+        require: true,
+    },
+    dribble:{
+        type:Number,
+        required : true,
+    },
+    speed:{
+        type:Number,
+        required : true,
+    },
+    shoot:{
+        type:Number,
+        required : true,
+    },
+    defence:{
+        type:Number,
+        required : true,
+    },
+    pass:{
+        type:Number,
+        required : true,
+    },
+    physical:{
+        type:Number,
+        required : true,
+    },
+    ova:{
+        type:Number,
+        required : true,
+    },
+
+},{
         versionKey: false // You should be aware of the outcome after set to false
     }
 );
